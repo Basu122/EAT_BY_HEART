b5aae157361956e7326de945a5cdf7b4a86a89a0
Basu122 <basuoffi02@gmail.com>
2024-08-29 22:51:45 +0530

    added project

---
+++ src/main/java/com/eat/by/heart/EatByHeartApplication.java
@@ -0,0 +1,13 @@
+package com.eat.by.heart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatByHeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatByHeartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eat/by/heart/EatByHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eat.by.heart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatByHeartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
